193f2d3801f31e9a5cafd9eb3c0b66071db6a1af
Adilkhan76 <112398888+Adilkhan76@users.noreply.github.com>
2022-12-17 12:19:17 +0530

    Add files via upload

---
+++ assign4.py
@@ -0,0 +1,30 @@
+#1. Write a python script to take your name as input from the user and then print it.
+print(input("write your name"))
+
+#2. Write a python script to take input from the user. Input must be a number.
+print(int(input("enter a number")))
+
+#3. Write a python script which takes two numbers from the user, then calculate their sum and display the result.
+print(int(input('enter a number'))+int(input('enter second number')))
+
+#4. Write a python script which takes the radius from the user and display area of a circle.
+print(3.14*int(input('enter radius'))**2)
+
+#5. Write a python script to calculate the square of a number. Number is entered by the user.
+print(int(input('enter a number'))**2)
+
+#6. Write a python script to calculate the area of Triangle. Number is entered by the user.
+print(0.5*int(input('enter base for Triangle'))*int(input('enter height for Triangle')))
+
+#7. Write a python script to calculate average of three numbers, entered by the user
+total=(int(input('enter number_1'))+int(input('enter number_2'))+int(input('enter number_3')))
+print(total/3)
+
+#8. Write a python script to calculate simple interest
+print('simple interest is',int(input('enter principle'))*int(input('enter interest rate'))*int(input('enter time')))
+
+#9. Write a python script to calculate the volume of a cuboid.
+print('Cuboid volume is ',int(input('enter l'))*int(input('enter b'))*int(input('enter h')))
+
+#10. Write a python script to calculate area of a rectangle
+print('area of rectangle is',int(input('enter l'))*int(input('enter b')))
