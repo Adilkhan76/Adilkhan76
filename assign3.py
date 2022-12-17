13a851903f14df9f8fd797b3146db9ab9d99e921
Adilkhan76 <112398888+Adilkhan76@users.noreply.github.com>
2022-12-17 12:10:28 +0530

    Add files via upload

---
+++ assign3.py
@@ -0,0 +1,37 @@
+#Assignment-3: Type Conversion
+'''1. Write a python script to convert a number into str type.
+number=12
+number2=str(number)
+print(type(number2))'''
+#2. Write a python script to print Unicode of the character ‘m’
+print(ord('m'))
+#3. Write a python script to print character representation of a given unicode 100.
+print(chr(100))
+
+#4. Write a python script to print any number and its binary equivalent
+num=12
+print(bin(num))
+
+#5. Write a python script to print any number and its octal equivalent.
+num=12
+print(oct(num))
+
+#6. Write a python script to print any number and its hexadecimal equivalent.
+num=100
+print(hex(num))
+
+#7. Write a python script to store binary number 1100101 in a variable and print it in decimal format.
+'''ab=1100101
+num3='0b'+str(ab)
+print(num3)'''
+
+'''8. Write a python script to store a hexadecimal number 2F in a variable and print it in octal format.
+hexadec=2F
+print(oct(hexadec))'''
+
+#9. Write a python script to store an octal number 125 in a variable and print it in binary format.
+octal=125
+octal='0o'+'125'
+print(bin(octal))
+'''10. Write a python script to add two numbers 25 (in octal) and 39 (in hexadecimal) and
+display the result in binary format.'''
