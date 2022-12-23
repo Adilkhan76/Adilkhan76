60dfac62336642bbdf13e806a6763f68ff55e21a
Adilkhan76 <112398888+Adilkhan76@users.noreply.github.com>
2022-12-23 17:45:00 +0530

    Assignment 8 Upload

---
+++ Assign8.py
@@ -0,0 +1,39 @@
+#1. Write a python script to print MySirG 5 times on the screen
+for i in range(5):
+    print('MySirG')
+
+#2. Write a python script to print first 10 natural numbers
+for i in range(10):
+    print(i,end=' ')
+#3. Write a python script to print first 10 natural numbers in reverse order
+for i in range(9,-1,-1):
+    print(i,end=' ')
+
+#4. Write a python script to print first 10 odd natural numbers
+for i in range(1,11):
+    print(2*i-1,end=' ')
+
+#5. Write a python script to print first 10 odd natural numbers in reverse order
+
+for i in range(10,0,-1):
+    print(2*i-1,end=' ')
+
+#6. Write a python script to print first 10 even natural numbers
+for i in range(1,11):
+    print(2*i,end=' ')
+
+#7. Write a python script to print first 10 even natural numbers in reverse order
+for i in range(10,0,-1):
+    print(2*i,end=' ')
+
+#8. Write a python script to print squares of first 10 natural numbers
+for i in range(1,11):
+    print(i**2,end=' ')
+
+#9. Write a python script to print cubes of first 10 natural numbers
+for i in range(1,11):
+    print(i**3,end=' ')
+
+#10. Write a python script to print first 10 multiples of 5
+for i in range(1,11):
+    print(i*5,end=' ')
